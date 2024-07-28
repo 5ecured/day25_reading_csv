9742d585712f873d766f8da507cb87be86123a14
Edward <onirotto7585@pm.me>
2024-07-28 15:46:10 +0700

    US states game finished

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
-# with open('./weather_data.csv') as weather_data:
-#     data = weather_data.readlines()
-#     print(data)
-#     when you print data, you get:
-#     ['day,temp,condition\n', 'Monday,12,Sunny\n', 'Tuesday,14,Rain\n', 'Wednesday,15,Rain\n', 'Thursday,14,Cloudy\n', 'Friday,21,Sunny\n', 'Saturday,22,Sunny\n', 'Sunday,24,Sunny']
-
-
-##### we can use the built-in CSV module from python #####
-
-# import csv
-
-# with open('./weather_data.csv') as weather_data:
-#     data = csv.reader(weather_data)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != 'temp':
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-    
-
-##### or we can use pandas package #####
-
+import turtle 
 import pandas
 
-data = pandas.read_csv('./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+screen = turtle.Screen()
+screen.title('US States game')
+image = './blank_states_img.gif'
+screen.addshape(image)
 
-gray_squirrels_count = len(data[data['Primary Fur Color'] == 'Gray'])
-red_squirrels_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
-black_squirrels_count = len(data[data['Primary Fur Color'] == 'Black'])
+turtle.shape(image)
 
-print(gray_squirrels_count)
-print(red_squirrels_count)
-print(black_squirrels_count)
+data = pandas.read_csv('50_states.csv')
+all_states = data.state.to_list()
+guessed_states = []
 
-#constructing a Pandas Data Frame
-data_dict = {
-    'Fur Color': ['Gray', 'Cinnamon', 'Black'],
-    'Count': [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
-}
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 States correct Guess the State', prompt='What\'s another state\'s name?').title()
 
-df = pandas.DataFrame(data_dict)
-df.to_csv('squirrel_count.csv')
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.item()), int(state_data.y.item()))
+        t.write(answer_state)
+
+screen.exitonclick()
